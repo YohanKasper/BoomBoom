5681fec46591e5111398f52115edc2a88791a83b
jules welcomme <jules.welcomme@gmail.com>
2020-08-17 15:42:54 +0200

    model offer

---
+++ db/migrate/20200817134235_create_offers.rb
@@ -0,0 +1,14 @@
+class CreateOffers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :offers do |t|
+      t.string :title
+      t.string :description
+      t.integer :price
+      t.date :date
+      t.user :references
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end

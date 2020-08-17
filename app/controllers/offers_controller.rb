08a2c687718c3a88c7edeefc9a6d509020743230
Valentin Tournant <valentin.tournant@gmail.com>
2020-08-17 18:11:38 +0200

    update and edit def plus view

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,4 +1,5 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: [:show, :edit, :update, :destroy]
   def index
     @offers = Offer.all
   end
@@ -10,13 +11,29 @@ class OffersController < ApplicationController
   def create
     @offer = Offer.new(offer_params)
     if @offer.save
-      redirect_to offer_path(@offer)
+      redirect_to offer_path(@offer), notice: 'Offer was successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+    # @offer = Offer.find(params[:id])
+  end
+
+  def update
+    if @offer.update(offer_params)
+      redirect_to offer_path(@offer), notice: 'Offer was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
 
   def offer_params
     params.require(:offer).permit(:title, :description, :price, :date, :category)

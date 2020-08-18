30df98e3f3c6454a3ca458ac366fb296cd74dc75
Madeleine Bizard <bizard.madeleine@gmail.com>
2020-08-18 11:43:53 +0200

    new users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_18_090745) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

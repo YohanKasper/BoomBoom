a529f4213216da8609e8e46f80e078b2e85d4355
Madeleine Bizard <bizard.madeleine@gmail.com>
2020-08-18 14:25:42 +0200

    the js bar

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,17 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+// const channels = require.context('.', true, /_channel\.js$/)
+// channels.keys().forEach(channels)
+
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["Costumes, Drinks, Lights, Speakers, Food Trucks...", "... All you need is BoomBoom!"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,13 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import { loadDynamicBannerText } from '../channels/index';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
+  if (document.querySelector('#banner-typed-text')) loadDynamicBannerText()
+
   // initSelect2();
 });

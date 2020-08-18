7a7f3e5654d30a6d3032e67a04cdd0c9ef6a0a3b
jules welcomme <jules.welcomme@gmail.com>
2020-08-18 15:53:51 +0200

    reparation

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -46,6 +46,6 @@ class OffersController < ApplicationController
   end
 
   def offer_params
-    params.require(:offer).permit(:title, :description, :price, :date, :category)
+    params.require(:offer).permit(:title, :description, :price, :date, :category, :photo)
   end
 end

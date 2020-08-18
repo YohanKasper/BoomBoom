7b647bad47b5793a96af1508620e2cc07f01f00f
Valentin Tournant <valentin.tournant@gmail.com>
2020-08-18 17:56:50 +0200

    creation of the page profil

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :users, only: [:show]
+
   resources :offers do
     resources :bookings, only: [:new, :create]
   end

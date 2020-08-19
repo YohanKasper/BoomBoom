9a6d0494191768450d9cf342bf79c9a35f29d19b
Valentin Tournant <valentin.tournant@gmail.com>
2020-08-19 12:16:23 +0200

    creation profil with current_user + model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def dashboard
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:show]
+  get 'dashboard', to: 'users#dashboard'
 
   resources :offers do
     resources :bookings, only: [:new, :create]

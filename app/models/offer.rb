46bf2469df3e51a82c8050276f4e4f78eb22c673
Yohan Franquelin <yranquelin@gmail.com>
2020-08-19 16:33:42 +0200

    clean

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true

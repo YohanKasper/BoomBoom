8d2ea7e4005c8a8390bc4c5c0c65705bc668e05a
Valentin Tournant <valentin.tournant@gmail.com>
2020-08-20 14:30:30 +0200

    push pg-search-gem

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -8,4 +8,11 @@ class Offer < ApplicationRecord
   validates :category, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+    against: [:title, :description],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
